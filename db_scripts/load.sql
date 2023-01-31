305fcc4f75181df4bb6b8f3a8396bc286e1db25e
ChloeM <meinshac@gmail.com>
2023-01-31 16:31:35 -0500

    add load scripts

--- db_scripts/load.sql
+++ db_scripts/load.sql
@@ -1,13 +1,13 @@
 -- import authors csv
 alter table authors set unlogged;
-\copy authors from './data/processed/ol_dump_authors.txt' delimiter E'\t' quote '|' csv;
+\i 'db_scripts/openlibrary-data-loader.sql'
 alter table authors set logged;
 
 \i 'db_scripts/tbl_authors_indexes.sql';
 
 -- import works csv
 alter table works set unlogged;
-\copy works from './data/processed/ol_dump_works.txt' delimiter E'\t' quote '|' csv;
+\i 'db_scripts/openlibrary-data-loader.sql'
 alter table works set logged;
 
 \i 'db_scripts/tbl_works_indexes.sql';
@@ -18,12 +18,12 @@ insert into author_works (author_key, work_key)
 select distinct author_key, work_key
 from (
     select
-        jsonb_array_elements(data->'authors')->'author'->>'key' as author_key, 
+        jsonb_array_elements(data->'authors')->'author'->>'key' as author_key,
         key as work_key
     from works
     where key is not null
     and data->'authors'->0->'author' is not null) authorship
-where author_key is not null 
+where author_key is not null
 and work_key is not null;
 alter table author_works set logged;
 
@@ -31,8 +31,8 @@ alter table author_works set logged;
 
 -- import editions csv
 alter table editions set unlogged;
-\copy editions from './data/processed/ol_dump_editions.txt' delimiter E'\t' quote '|' csv;
-
+-- \copy editions from './data/processed/ol_dump_editions.txt' delimiter E'\t' quote '|' csv;
+\i 'db_scripts/openlibrary-data-loader.sql'
 alter table editions
 add column work_key text;
 
@@ -45,7 +45,7 @@ alter table editions set logged;
 -- set isbn for edition_isbns from the embedded json
 alter table edition_isbns set unlogged;
 insert into edition_isbns (edition_key, isbn)
-select 
+select
     distinct edition_key,
     isbn
 from (select
