c94df0820a1baa2a386ce00e8a40e43a3170c4c7
Dave <8709533+DaveBathnes@users.noreply.github.com>
2025-04-29 21:26:20 +0100

    Tidying up and adding indexes (#18)

--- db_scripts/tbl_editions_indexes.sql
+++ db_scripts/tbl_editions_indexes.sql
@@ -3,3 +3,7 @@ alter table editions cluster on cuix_editions_key;
 
 create index ix_editions_workkey on editions (work_key);
 create index ix_editions_data on editions using gin (data jsonb_path_ops);
+
+-- index title and subtitle from the jsonb data
+create index ix_editions_title on editions using gin ((data->>'title') gin_trgm_ops);
+create index ix_editions_subtitle on editions using gin ((data->>'subtitle') gin_trgm_ops);
--- db_scripts/tbl_works_indexes.sql
+++ db_scripts/tbl_works_indexes.sql
@@ -2,3 +2,7 @@ create unique index cuix_works_key on works (key);
 alter table works cluster on cuix_works_key;
 
 create index ix_works_data on works using gin (data jsonb_path_ops);
+
+-- index title and subtitle from jsonb data
+create index ix_works_title on works using gin ((data->>'title') gin_trgm_ops);
+create index ix_works_subtitle on works using gin ((data->>'subtitle') gin_trgm_ops);
