95b26762a9e932fa74778ca00c17fdfdfd2c2db8
Dave Rowe <8709533+DaveBathnes@users.noreply.github.com>
2026-01-30 19:11:57 +0000

    Changing indexes to not index whole data column

--- db_scripts/load.sql
+++ db_scripts/load.sql
@@ -1,23 +1,22 @@
-select now();
 -- import authors csv
+select now() as "Importing authors";
 alter table authors set unlogged;
 \i 'db_scripts/openlibrary-data-loader.sql'
 alter table authors set logged;
 
-select now();
+select now() as "Creating author indexes";
 \i 'db_scripts/tbl_authors_indexes.sql';
 
-select now();
 -- import works csv
+select now() as "Importing works";
 alter table works set unlogged;
 \i 'db_scripts/openlibrary-data-loader.sql'
 alter table works set logged;
 
-select now();
+select now() as "Creating works indexes";
 \i 'db_scripts/tbl_works_indexes.sql';
 
-select now();
--- set author and work_key for author_works from the data embedded in works
+select now() as "Setting author and work_key in author_works";
 alter table author_works set unlogged;
 insert into author_works (author_key, work_key)
 select distinct author_key, work_key
@@ -32,28 +31,28 @@ where author_key is not null
 and work_key is not null;
 alter table author_works set logged;
 
-select now();
+select now() as "Creating author_works indexes";
 \i 'db_scripts/tbl_author_works_indexes.sql';
 
-select now();
 -- import editions csv
+select now() as "Importing editions";
 alter table editions set unlogged;
-select now();
 \i 'db_scripts/openlibrary-data-loader.sql'
+
+select now() as "Setting work_key in editions";
 alter table editions
 add column work_key text;
-
-select now();
 update editions
 set work_key = data->'works'->0->>'key';
 alter table editions set logged;
 
-select now();
-\i 'db_scripts/tbl_editions_indexes.sql';
-select now();
--- set isbn for edition_isbns from the embedded json
 
-select now();
+select now() as "Creating editions indexes";
+\i 'db_scripts/tbl_editions_indexes.sql';
+
+
+-- set isbn for edition_isbns from the embedded json
+select now() as "Setting isbns from editions jsonb data";
 alter table edition_isbns set unlogged;
 insert into edition_isbns (edition_key, isbn)
 select
@@ -83,7 +82,7 @@ from (
 alter table edition_isbns set logged;
 
 
-select now();
 -- create isbn indexes
+select now() as "Creating edition_isbns indexes";
 \i 'db_scripts/tbl_edition_isbns_indexes.sql';
 select now();
--- db_scripts/tbl_editions_indexes.sql
+++ db_scripts/tbl_editions_indexes.sql
@@ -2,7 +2,10 @@ create unique index cuix_editions_key on editions (key);
 alter table editions cluster on cuix_editions_key;
 
 create index ix_editions_workkey on editions (work_key);
-create index ix_editions_data on editions using gin (data jsonb_path_ops);
+
+create index ix_editions_isbn on editions using gin ((data->'isbn') jsonb_path_ops);
+create index ix_editions_isbn13 on editions using gin ((data->'isbn_13') jsonb_path_ops);
+create index ix_editions_isbn10 on editions using gin ((data->'isbn_10') jsonb_path_ops);
 
 -- index title and subtitle from the jsonb data
 create index ix_editions_title on editions using gin ((data->>'title') gin_trgm_ops);
